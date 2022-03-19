5818f57ed5ea79013049f4a1520b5f1899037bcf
Alejandro <jeffersonaleandro27@gmail.com>
2022-03-19 02:51:37 -0500

    d

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const qrcode = require('qrcode-terminal');
 const { Client } = require('whatsapp-web.js');
 // const fs = require('fs');
 const path = require('path');
-let client;
+let client = new Client();
 let sessionData;
 
 const SESSION_FILE_PATH = path.join(__dirname, './session.json');
@@ -29,50 +29,50 @@ const SESSION_FILE_PATH = path.join(__dirname, './session.json');
 // };
 
 // Estas fn genere el QR
-const withOutSession = () => {
-  console.log('NO TENEMOS SESION INICIADA');
-  client = new Client();
+// const withOutSession = () => {
+//   console.log('NO TENEMOS SESION INICIADA');
+//   client = new Client();
 
-  client.on('qr', (qr) => {
-    qrcode.generate(qr, { small: true });
-  });
+//   client.on('qr', (qr) => {
+//     qrcode.generate(qr, { small: true });
+//   });
 
-  client.on('authenticated', async (session) => {
-    //Guardamos las credencias de session para usar luego
-    sessionData = await session;
-    console.log(sessionData, 'Esta es la sesion!!!');
+//   client.on('authenticated', async (session) => {
+//     //Guardamos las credencias de session para usar luego
+//     sessionData = await session;
+//     console.log(sessionData, 'Esta es la sesion!!!');
 
-    // fs.writeFile(SESSION_FILE_PATH, JSON.stringify(sessionData), (err) => {
-    //   if (err) {
-    //     console.log(err);
-    //   }
-    // });
-  });
+//     // fs.writeFile(SESSION_FILE_PATH, JSON.stringify(sessionData), (err) => {
+//     //   if (err) {
+//     //     console.log(err);
+//     //   }
+//     // });
+//   });
 
-  client.on('ready', () => {
-    console.log('Client is ready!');
-  });
+//   client.on('ready', () => {
+//     console.log('Client is ready!');
+//   });
 
-  client.initialize();
-  listenMessage();
-};
+//   client.initialize();
+//   listenMessage();
+// };
 
-const sendMessage = async (to, message) => {
-  client.sendMessage(to, message);
-};
+// const sendMessage = async (to, message) => {
+//   client.sendMessage(to, message);
+// };
 
-//fn handle lisent message
-const listenMessage = () => {
-  const nroPrincipal = '0984266244';
-  const enlaceDirecto = 'https://n9.cl/swry0 ';
-  const msgSend = `Hola 🤖, ¿Tienes alguna duda? \n \n Agradecemos si nos enviás un mensaje a nuestro numero principal de WhatsApp, estaremos gustosos de ayudarte. \n \n • Nro principal: ${nroPrincipal} \n • Enlace directo: ${enlaceDirecto}`;
+// //fn handle lisent message
+// const listenMessage = () => {
+//   const nroPrincipal = '0984266244';
+//   const enlaceDirecto = 'https://n9.cl/swry0 ';
+//   const msgSend = `Hola 🤖, ¿Tienes alguna duda? \n \n Agradecemos si nos enviás un mensaje a nuestro numero principal de WhatsApp, estaremos gustosos de ayudarte. \n \n • Nro principal: ${nroPrincipal} \n • Enlace directo: ${enlaceDirecto}`;
 
-  client.on('message', (msg) => {
-    const { from, to, body } = msg;
-    client.sendMessage(from, msgSend);
-    console.log(from, to, body);
-  });
-};
+//   client.on('message', (msg) => {
+//     const { from, to, body } = msg;
+//     client.sendMessage(from, msgSend);
+//     console.log(from, to, body);
+//   });
+// };
 
 // fs.existsSync(SESSION_FILE_PATH) ? withSession() :
 withOutSession();
